ed7a98bc737bc81de9a74f19672cc5b9fc69e196
huapc <huapc@outlook.com>
2020-03-09 21:06:01 +0800

    commit

--- operation/models.py
+++ operation/models.py
@@ -42,3 +42,18 @@ class UserCourse(models.Model):
     class Meta:
         verbose_name = '用户课程'
         verbose_name_plural = verbose_name
+
+class UserFavorite(models.Model):
+    FAV_TYPE = (
+        (1, '课程'),
+        (2, '课程机构'),
+        (3, '讲师'),
+    )
+    user = models.ForeignKey(UserProfile, verbose_name='用户', on_delete=models.CASCADE)
+    fav_id = models.IntegerField('数据id', default=0)
+    fav_type = models.IntegerField('收藏类型', choices=FAV_TYPE, default=1)
+    add_time = models.DateTimeField('添加时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '用户收藏'
+        verbose_name_plural = verbose_name
